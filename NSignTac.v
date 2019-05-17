37b42bddf31173958016b11b17a277240cb585fe
Sora Chen <210at85@gmail.com>
2019-05-17 14:01:34 +0800

    [Nat|N|R]GroundTac.v [Nat|N|Z|R]SignTac.v

--- NSignTac.v
+++ NSignTac.v
@@ -7,50 +7,53 @@ Open Scope N_scope.
 
 
 Ltac Nsign_tac :=
- repeat (apply Nmult_le_compat_l || apply Nmult_lt_compat_l ||
-              apply Nmult_ge_compat_l || apply Nmult_gt_compat_l ||
-              apply Nlt_mult_0 || apply Ngt_mult_0); auto.
+  repeat (apply Nmult_le_compat_l || apply Nmult_lt_compat_l ||
+          apply Nmult_ge_compat_l || apply Nmult_gt_compat_l ||
+          apply Nlt_mult_0 || apply Ngt_mult_0); auto.
 
 Ltac hyp_Nsign_tac H :=
   match type of H with
-   0 <= _ => clear H
-| (?X1 <= 0)%N => generalize (Nle_0_eq_0 _ H); clear H; intros H; subst X1
-|  (?X1 * _ <= ?X1 * _)%N =>
-             let s1 := fresh "NS" in
-                   (assert (s1: 0 < X1); [Nsign_tac; fail |
-                   generalize (Nmult_le_compat_rev_l _ _ _ H s1);
-                   clear H s1; intros H])
-|   (0  < ?X1 * _)%N  =>
-              let s1 := fresh "NS" in
-                   (generalize (Nlt_mult_rev_0_l _ _ H);
-                    generalize (Nlt_mult_rev_0_r _ _ H); clear H;
-                    intros H s1; hyp_Nsign_tac s1; hyp_Nsign_tac H)
-|  (?X1 < 0)%N => absurd (~ (X1 < 0)%N); auto
-| (?X1 * _  < ?X1 * _)%N =>
-              let s1 := fresh "NS" in
-                   (generalize (Nmult_lt_compat_rev_l1 _ _ _ H);
-                    generalize (Nmult_lt_compat_rev_l2 _ _ _ H); clear H;
-                    intros H s1; hyp_Nsign_tac s1; hyp_Nsign_tac H)
-|  (?X1 >= 0)%N => clear H
-| (0 >= ?X1)%N  => generalize (Nge_0_eq_0 _ H); clear H; intros H; subst X1
-|  (?X1 * _ >= ?X1 * _)%N =>
-             let s1 := fresh "NS" in
-                   (assert (s1: (0 < X1)%N); [Nsign_tac; fail |
-                   generalize (Nmult_ge_compat_rev_l _ _ _ H s1);
-                   clear H s1; intros H])
-|  (?X1 * _ > 0 )%N =>
-              let s1 := fresh "NS" in
-                   (generalize (Ngt_mult_rev_0_l _ _ H);
-                    generalize (Ngt_mult_rev_0_r _ _ H); clear H;
-                    intros H s1; hyp_Nsign_tac s1; hyp_Nsign_tac H)
-|  (0 > ?X1)%N => absurd (~ (0 > X1)%N); auto with arith
-| (?X1 * _  > ?X1 * _)%N =>
-              let s1 := fresh "NS" in
-                   (generalize (Nmult_gt_compat_rev_l1 _ _ _ H);
-                    generalize (Nmult_gt_compat_rev_l2 _ _ _ H); clear H;
-                    intros H s1; hyp_Nsign_tac s1; hyp_Nsign_tac H)
-  |  _ => (let u := type of H in (clear H; assert (H: u); [auto; fail | clear H]) || idtac)
-
-   end.
+  | 0 <= _ => clear H
+  | (?X1 <= 0)%N => generalize (Nle_0_eq_0 _ H); clear H; intros H; subst X1
+  | (?X1 * _ <= ?X1 * _)%N =>
+    let s1 := fresh "NS" in
+    assert (s1: 0 < X1);
+    [ Nsign_tac; fail
+    | generalize (Nmult_le_compat_rev_l _ _ _ H s1);
+      clear H s1; intros H ]
+  | (0 < ?X1 * _)%N =>
+    let s1 := fresh "NS" in
+    generalize (Nlt_mult_rev_0_l _ _ H);
+    generalize (Nlt_mult_rev_0_r _ _ H); clear H;
+    intros H s1; hyp_Nsign_tac s1; hyp_Nsign_tac H
+  | (?X1 < 0)%N => absurd (~ (X1 < 0)%N); auto
+  | (?X1 * _ < ?X1 * _)%N =>
+    let s1 := fresh "NS" in
+    generalize (Nmult_lt_compat_rev_l1 _ _ _ H);
+    generalize (Nmult_lt_compat_rev_l2 _ _ _ H); clear H;
+    intros H s1; hyp_Nsign_tac s1; hyp_Nsign_tac H
+  | (?X1 >= 0)%N => clear H
+  | (0 >= ?X1)%N => generalize (Nge_0_eq_0 _ H); clear H; intros H; subst X1
+  | (?X1 * _ >= ?X1 * _)%N =>
+    let s1 := fresh "NS" in
+    assert (s1: (0 < X1)%N);
+    [ Nsign_tac; fail
+    | generalize (Nmult_ge_compat_rev_l _ _ _ H s1);
+      clear H s1; intros H ]
+  | (?X1 * _ > 0 )%N =>
+    let s1 := fresh "NS" in
+    generalize (Ngt_mult_rev_0_l _ _ H);
+    generalize (Ngt_mult_rev_0_r _ _ H); clear H;
+    intros H s1; hyp_Nsign_tac s1; hyp_Nsign_tac H
+  | (0 > ?X1)%N => absurd (~ (0 > X1)%N); auto with arith
+  | (?X1 * _ > ?X1 * _)%N =>
+    let s1 := fresh "NS" in
+    generalize (Nmult_gt_compat_rev_l1 _ _ _ H);
+    generalize (Nmult_gt_compat_rev_l2 _ _ _ H); clear H;
+    intros H s1; hyp_Nsign_tac s1; hyp_Nsign_tac H
+  | _ =>
+    let u := type of H in
+    (clear H; assert (H : u); [auto; fail | clear H]) || idtac
+  end.
 
 Close Scope N_scope.
--- NatSignTac.v
+++ NatSignTac.v
@@ -1,60 +1,64 @@
-(* Ugly tacty to resolve sign condition for Z *)
+(* Ugly tacty to resolve sign condition for nat *)
 Require Import NatAux.
 Require Export NatGroundTac.
 
 
 Ltac nsign_tac :=
- repeat (apply mult_le_compat_l || apply mult_lt_compat_l ||
-              apply mult_ge_compat_l || apply mult_gt_compat_l ||
-              apply lt_mult_0 || apply gt_mult_0); auto with arith.
+  repeat (apply mult_le_compat_l || apply mult_lt_compat_l ||
+          apply mult_ge_compat_l || apply mult_gt_compat_l ||
+          apply lt_mult_0 || apply gt_mult_0); auto with arith.
 
 Ltac hyp_nsign_tac H :=
   match type of H with
-   0 <= _ => clear H
-| ?X1 <= 0 => generalize (le_0_eq_0 _ H); clear H; intros H; subst X1
-|  ?X1 * _ <= ?X1 * _ =>
-             let s1 := fresh "NS" in
-                   (assert (s1: 0 < X1); [nsign_tac; fail |
-                   generalize (mult_le_compat_rev_l _ _ _ H s1);
-                   clear H s1; intros H])
-|   0  < ?X1 * _ =>
-              let s1 := fresh "NS" in
-                   (generalize (lt_mult_rev_0_l _ _ H);
-                    generalize (lt_mult_rev_0_r _ _ H); clear H;
-                    intros H s1; hyp_nsign_tac s1; hyp_nsign_tac H)
-|  ?X1 < 0 => absurd (~ (X1 < 0)); auto with arith
-| ?X1 * _  < ?X1 * _ =>
-              let s1 := fresh "NS" in
-                   (generalize (mult_lt_compat_rev_l1 _ _ _ H);
-                    generalize (mult_lt_compat_rev_l2 _ _ _ H); clear H;
-                    intros H s1; hyp_nsign_tac s1; hyp_nsign_tac H)
-|  ?X1 >= 0 => clear H
-| 0 >= ?X1  => generalize (le_0_eq_0 _ H); clear H; intros H; subst X1
-|  ?X1 * _ >= ?X1 * _ =>
-             let s1 := fresh "NS" in
-                   (assert (s1: 0 < X1); [nsign_tac; fail |
-                   generalize (mult_ge_compat_rev_l _ _ _ H s1);
-                   clear H s1; intros H])
-|  ?X1 * _ > 0 =>
-              let s1 := fresh "NS" in
-                   (generalize (gt_mult_rev_0_l _ _ H);
-                    generalize (gt_mult_rev_0_r _ _ H); clear H;
-                    intros H s1; hyp_nsign_tac s1; hyp_nsign_tac H)
-|  0 > ?X1 => absurd (~ (0 > X1)); auto with arith
-| ?X1 * _  > ?X1 * _ =>
-              let s1 := fresh "NS" in
-                   (generalize (mult_gt_compat_rev_l1 _ _ _ H);
-                    generalize (mult_gt_compat_rev_l2 _ _ _ H); clear H;
-                    intros H s1; hyp_nsign_tac s1; hyp_nsign_tac H)
-  |  _ => (let u := type of H in (clear H; assert (H: u); [auto with arith; fail | clear H]) || idtac)
-
-   end.
+  | 0 <= _ => clear H
+  | ?X1 <= 0 => generalize (le_0_eq_0 _ H); clear H; intros H; subst X1
+  | ?X1 * _ <= ?X1 * _ =>
+    let s1 := fresh "NS" in
+    assert (s1: 0 < X1);
+    [ nsign_tac; fail
+    | generalize (mult_le_compat_rev_l _ _ _ H s1);
+      clear H s1; intros H ]
+  |  0 < ?X1 * _ =>
+     let s1 := fresh "NS" in
+     generalize (lt_mult_rev_0_l _ _ H);
+     generalize (lt_mult_rev_0_r _ _ H); clear H;
+     intros H s1; hyp_nsign_tac s1; hyp_nsign_tac H
+  | ?X1 < 0 => absurd (~ (X1 < 0)); auto with arith
+  | ?X1 * _ < ?X1 * _ =>
+    let s1 := fresh "NS" in
+    generalize (mult_lt_compat_rev_l1 _ _ _ H);
+    generalize (mult_lt_compat_rev_l2 _ _ _ H); clear H;
+    intros H s1; hyp_nsign_tac s1; hyp_nsign_tac H
+  | ?X1 >= 0 => clear H
+  | 0 >= ?X1 => generalize (le_0_eq_0 _ H); clear H; intros H; subst X1
+  | ?X1 * _ >= ?X1 * _ =>
+    let s1 := fresh "NS" in
+    assert (s1: 0 < X1);
+    [ nsign_tac; fail
+    | generalize (mult_ge_compat_rev_l _ _ _ H s1);
+      clear H s1; intros H ]
+  | ?X1 * _ > 0 =>
+    let s1 := fresh "NS" in
+    generalize (gt_mult_rev_0_l _ _ H);
+    generalize (gt_mult_rev_0_r _ _ H); clear H;
+    intros H s1; hyp_nsign_tac s1; hyp_nsign_tac H
+  | 0 > ?X1 => absurd (~ (0 > X1)); auto with arith
+  | ?X1 * _ > ?X1 * _ =>
+    let s1 := fresh "NS" in
+    generalize (mult_gt_compat_rev_l1 _ _ _ H);
+    generalize (mult_gt_compat_rev_l2 _ _ _ H); clear H;
+    intros H s1; hyp_nsign_tac s1; hyp_nsign_tac H
+  | _ =>
+    let u := type of H in
+    (clear H; assert (H : u); [auto with arith; fail | clear H]) || idtac
+  end.
 
 (* Test *)
 Section Test.
 
 Let hyp_test : forall a b c d e,
-  0 <= a -> 0 < a -> a * b <= a * c  -> b * a <= b * c -> d <= 0 -> e < 0 -> d = 0.
+  0 <= a -> 0 < a -> a * b <= a * c -> b * a <= b * c -> d <= 0 -> e < 0 -> d = 0.
+Proof.
 intros a b c d e H H1 H2 H3 H4 H5.
 (* H should disappear *)
 hyp_nsign_tac H.
@@ -68,9 +72,9 @@ hyp_nsign_tac H4.
 hyp_nsign_tac H5.
 Qed.
 
-
 Let hyp_test1 : forall a b c d e,
-  a >= 0 -> a > 0 -> a * b > a * c  -> b * a >= b * c -> 0 >= d -> 0 > e -> d = 0.
+  a >= 0 -> a > 0 -> a * b > a * c -> b * a >= b * c -> 0 >= d -> 0 > e -> d = 0.
+Proof.
 intros a b c d e H H1 H2 H3 H4 H5.
 (* H should disappear *)
 hyp_nsign_tac H.
