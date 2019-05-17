cf64b34134c590d13299226b662d9a73b45aa6ba
Sora Chen <210at85@gmail.com>
2019-05-17 14:02:44 +0800

    PolTac.v ReplaceTest.v

--- ReplaceTest.v
+++ ReplaceTest.v
@@ -1,20 +1,237 @@
 Require Import PolTac.
+Require Import Arith.
+Require Import NArith.
 Require Import ZArith.
+Require Import Reals.
 
-(* Test for Z *)
-Open Scope  Z_scope.
+(* Tests for Nat *)
+Open Scope nat_scope.
+
+Goal forall a b c d, a + c = d -> b = c -> a + b + c = c + d.
+Proof.
+intros.
+polr (a + c = d).
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, 0 = d -> a + b = 0 -> a + b + c = c + d.
+Proof.
+intros.
+polr (d = 0).
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + c <= d -> b <= c -> a + b + c <= c + d.
+Proof.
+intros.
+polr (a + c < d).
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + b <= 0 -> 0 <= d -> a + b + c <= c + d.
+Proof.
+intros.
+polrx (0 < d) P.R P.R 1%Z.
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + c <= d -> b <= c -> a + b + c <= c + d.
+Proof.
+intros.
+polr (a + c < d).
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + b >= 0 -> 0 >= d -> a + b + c >= c + d.
+Proof.
+intros.
+polrx (0 < d) P.R P.R 1%Z.
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + c < d -> b <= c -> a + b + c < c + d.
+Proof.
+intros.
+polr (a + c < d).
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + b <= 0 -> 0 < d -> a + b + c < c + d.
+Proof.
+intros.
+polrx (0 < d) P.R P.R 1%Z.
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + c > d -> c <= b -> a + b + c > c + d.
+Proof.
+intros.
+(* NatPolR.npolrx (a + c < d) P.L P.L 1%Z. *)
+polr (a + c < d).
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, 0 <= a + b -> 0 > d -> a + b + c > c + d.
+Proof.
+intros.
+polrx (0 < d) P.R P.R 1%Z.
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Close Scope nat_scope.
+
+
+(* Tests for N *)
+Open Scope N_scope.
+
+Goal forall a b c d, a + c = d -> b = c -> a + b + c = c + d.
+Proof.
+intros.
+polr (a + c = d).
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, 0 = d -> a + b = 0 -> a + b + c = c + d.
+Proof.
+intros.
+polr (d = 0).
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + c <= d -> b <= c -> a + b + c <= c + d.
+Proof.
+intros.
+polr (a + c < d).
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + b <= 0 -> 0 <= d -> a + b + c <= c + d.
+Proof.
+intros.
+polrx (0 < d) P.R P.R 1%Z.
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + c >= d -> b >= c -> a + b + c >= c + d.
+Proof.
+intros.
+polr (a + c < d).
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + b >= 0 -> 0 >= d -> a + b + c >= c + d.
+Proof.
+intros.
+polrx (0 < d) P.R P.R 1%Z.
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + c < d -> b <= c -> a + b + c < c + d.
+Proof.
+intros.
+polr (a + c < d).
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + b <= 0 -> 0 < d -> a + b + c < c + d.
+Proof.
+intros.
+polrx (0 < d) P.R P.R 1%Z.
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, a + c > d -> c <= b -> a + b + c > c + d.
+Proof.
+intros.
+polr (a + c < d).
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Goal forall a b c d, 0 <= a + b -> 0 > d -> a + b + c > c + d.
+Proof.
+intros.
+polrx (0 < d) P.R P.R 1%Z.
+pols.
+auto.
+pols.
+auto.
+Qed.
+
+Close Scope N_scope.
+
+
+(* Tests for Z *)
+Open Scope Z_scope.
 
 Ltac cg g := match goal with |- g => idtac end.
 
-
 Goal forall a b c d, a + c = d -> b + d = c + d -> a + b + c = c + d.
+Proof.
 intros a b c d H1 H2.
 polr H1.
-rewrite H1; auto. 
+rewrite H1; auto.
 auto.
 Qed.
 
 Goal forall a b c d, d = 0 -> a + b + c = c + 0 -> a + b + c = c + d.
+Proof.
 intros a b c d H1 H2.
 polr H1.
 rewrite H1; auto.
@@ -22,6 +239,7 @@ auto.
 Qed.
 
 Goal forall a b c d, a + c <= d -> b <= c -> a + b + c <= c + d.
+Proof.
 intros a b c d H1 H2.
 polr H1.
 pols.
@@ -30,7 +248,8 @@ pols.
 auto.
 Qed.
 
-Goal forall a b c d,  a + b  <= 0 -> 0 <= d -> a + b + c <= c + d.
+Goal forall a b c d, a + b <= 0 -> 0 <= d -> a + b + c <= c + d.
+Proof.
 intros.
 polrx (0 < d) P.R P.R 1%Z.
 pols.
@@ -40,6 +259,7 @@ auto.
 Qed.
 
 Goal forall a b c d, a + c >= d -> b >= c -> a + b + c >= c + d.
+Proof.
 intros.
 polr (a + c < d).
 pols.
@@ -49,6 +269,7 @@ auto.
 Qed.
 
 Goal forall a b c d, a + b >= 0 -> 0 >= d -> a + b + c >= c + d.
+Proof.
 intros.
 polrx (0 < d) P.R P.R 1%Z.
 pols.
@@ -57,8 +278,8 @@ pols.
 auto.
 Qed.
 
-
 Goal forall a b c d, a + c < d -> b <= c -> a + b + c < c + d.
+Proof.
 intros.
 polr (a + c < d).
 pols.
@@ -68,6 +289,7 @@ auto.
 Qed.
 
 Goal forall a b c d, a + b <= 0 -> 0 < d -> a + b + c < c + d.
+Proof.
 intros.
 polrx (0 < d) P.R P.R 1%Z.
 pols.
@@ -77,6 +299,7 @@ auto.
 Qed.
 
 Goal forall a b c d, a + c > d -> c <= b -> a + b + c > c + d.
+Proof.
 intros.
 polr (a + c < d).
 pols.
@@ -86,6 +309,7 @@ auto.
 Qed.
 
 Goal forall a b c d, 0 <= a + b -> 0 > d -> a + b + c > c + d.
+Proof.
 intros.
 polrx (0 < d) P.R P.R 1%Z.
 pols.
@@ -94,11 +318,14 @@ pols.
 auto.
 Qed.
 
-(* Test for N *)
-Require Import NAux.
-Open Scope  N_scope.
+Close Scope Z_scope.
+
+
+(* Tests for R *)
+Open Scope R_scope.
 
 Goal forall a b c d, a + c = d -> b = c -> a + b + c = c + d.
+Proof.
 intros.
 polr (a + c = d).
 pols.
@@ -108,6 +335,7 @@ auto.
 Qed.
 
 Goal forall a b c d, 0 = d -> a + b = 0 -> a + b + c = c + d.
+Proof.
 intros.
 polr (d = 0).
 pols.
@@ -117,6 +345,7 @@ auto.
 Qed.
 
 Goal forall a b c d, a + c <= d -> b <= c -> a + b + c <= c + d.
+Proof.
 intros.
 polr (a + c < d).
 pols.
@@ -126,6 +355,7 @@ auto.
 Qed.
 
 Goal forall a b c d, a + b <= 0 -> 0 <= d -> a + b + c <= c + d.
+Proof.
 intros.
 polrx (0 < d) P.R P.R 1%Z.
 pols.
@@ -135,6 +365,7 @@ auto.
 Qed.
 
 Goal forall a b c d, a + c >= d -> b >= c -> a + b + c >= c + d.
+Proof.
 intros.
 polr (a + c < d).
 pols.
@@ -144,6 +375,7 @@ auto.
 Qed.
 
 Goal forall a b c d, a + b >= 0 -> 0 >= d -> a + b + c >= c + d.
+Proof.
 intros.
 polrx (0 < d) P.R P.R 1%Z.
 pols.
@@ -153,6 +385,7 @@ auto.
 Qed.
 
 Goal forall a b c d, a + c < d -> b <= c -> a + b + c < c + d.
+Proof.
 intros.
 polr (a + c < d).
 pols.
@@ -162,6 +395,7 @@ auto.
 Qed.
 
 Goal forall a b c d, a + b <= 0 -> 0 < d -> a + b + c < c + d.
+Proof.
 intros.
 polrx (0 < d) P.R P.R 1%Z.
 pols.
@@ -169,208 +403,19 @@ auto.
 pols.
 auto.
 Qed.
-
-Goal forall a b c d, a + c > d -> c <= b -> a + b + c > c + d.
-intros.
-polr (a + c < d).
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, 0 <= a + b -> 0 > d -> a + b + c > c + d.
-intros.
-polrx (0 < d) P.R P.R 1%Z.
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Require Import Arith.
-(* Test for Nat *)
-Open Scope  nat_scope.
-
-Goal forall a b c d, a + c = d -> b = c -> a + b + c = c + d.
-intros.
-polr (a + c = d).
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, 0 = d -> a + b = 0 -> a + b + c = c + d.
-intros.
-polr (d = 0).
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, a + c <= d -> b <= c -> a + b + c <= c + d.
-intros.
-polr (a + c < d).
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, a + b <= 0 -> 0 <= d -> a + b + c <= c + d.
-intros.
-polrx (0 < d) P.R P.R 1%Z.
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, a + c <= d -> b <= c -> a + b + c <= c + d.
-intros.
-polr (a + c < d).
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, a + b >= 0 -> 0 >= d -> a + b + c >= c + d.
-intros.
-polrx (0 < d) P.R P.R 1%Z. 
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, a + c < d -> b <= c -> a + b + c < c + d.
-intros.
-polr (a + c < d).
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, a + b <= 0 -> 0 < d -> a + b + c < c + d.
-intros.
-polrx (0 < d) P.R P.R 1%Z.
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, a + c > d -> c <= b -> a + b + c > c + d.
-intros.
-(*
-NatPolR.npolrx (a + c < d) P.L P.L 1%Z.
-*)
-polr (a + c < d).
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, 0 <= a + b -> 0 > d -> a + b + c > c + d.
-intros.
-polrx (0 < d) P.R P.R 1%Z.
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-(* Test for R *)
-Require Import Reals.
-Open Scope  R_scope.
-
-Goal forall a b c d, a + c = d -> b = c -> a + b + c = c + d.
-intros.
-polr (a + c = d).
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, 0 = d -> a + b = 0 -> a + b + c = c + d.
-intros.
-polr (d = 0).
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, a + c <= d -> b <= c -> a + b + c <= c + d.
-intros.
-polr (a + c < d).
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, a + b <= 0 -> 0 <= d -> a + b + c <= c + d.
-intros.
-polrx (0 < d) P.R P.R 1%Z.
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, a + c >= d -> b >= c -> a + b + c >= c + d.
-intros.
-polr (a + c < d).
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, a + b >= 0 -> 0 >= d -> a + b + c >= c + d.
-intros.
-polrx (0 < d) P.R P.R 1%Z.
-pols.
-auto.
-pols.
-auto.
-Qed.
-
-Goal forall a b c d, a + c < d -> b <= c -> a + b + c < c + d.
-intros.
-polr (a + c < d).
-pols.
-auto.
-pols.
-auto.
-Qed.
- 
-Goal forall a b c d, a + b <= 0 -> 0 < d -> a + b + c < c + d.
-intros.
-polrx (0 < d) P.R P.R 1%Z.
-pols.
-auto.
-pols.
-auto.
-Qed. 
 
 Goal forall a b c d, a + c > d -> b >= c -> a + b + c > c + d.
+Proof.
 intros.
 polr (a + c < d).
 pols.
 auto.
 pols.
 auto.
-Qed. 
+Qed.
 
 Goal forall a b c d, a + b >= 0 -> 0 > d -> a + b + c > c + d.
+Proof.
 intros.
 polrx (0 < d) P.R P.R 1%Z.
 pols.
@@ -378,3 +423,5 @@ auto.
 pols.
 auto.
 Qed.
+
+Close Scope Z_scope.
