1bf58c2c81bbac5d062d4dbfc04336b38455171b
Sora Chen <210at85@gmail.com>
2019-05-17 14:02:55 +0800

    [Nat|N|Z|R]ex.v

--- Rex.v
+++ Rex.v
@@ -2,53 +2,69 @@ Require Import Reals.
 Require Import PolTac.
 
 Open Scope R_scope.
- 
-Theorem pols_test1: forall (x y : R), x < y ->  (x + x < y + x).
+
+Theorem pols_test1 x y :
+  x < y -> x + x < y + x.
+Proof.
 intros.
 pols.
 auto.
 Qed.
- 
-Theorem pols_test2: forall (x y : R), 0 < y ->  (x  < x + y).
+
+Theorem pols_test2 x y :
+  0 < y -> x < x + y.
+Proof.
 intros.
 pols.
 auto.
 Qed.
- 
-Theorem pols_test3: forall (x y : R), 0 < y * y ->  ((x + y) * (x - y) < x * x).
+
+Theorem pols_test3 x y :
+  0 < y * y ->
+  (x + y) * (x - y) < x * x.
+Proof.
 intros.
 pols.
 auto with real.
 Qed.
 
-Theorem pols_test4:
- forall (x y : R),
- x * x  < y * y ->  ((x + y) * (x + y) < 2 * (x * y + y * y)).
-intros.
-pols.
-auto.
-Qed.
- 
-Theorem pols_test5:
- forall (x y z : R),
- x * (z + 2) < y * (2 * x + 1)->  (x * ((z + y) + 2) < y * (3 * x + 1)).
+Theorem pols_test4 x y :
+  x * x < y * y ->
+  (x + y) * (x + y) < 2 * (x * y + y * y).
+Proof.
 intros.
 pols.
 auto.
 Qed.
 
-Theorem polf_test1: forall x y, (0 <= x -> 1 <= y -> x  <=  x  * y)%R.
+Theorem pols_test5 x y z :
+  x * (z + 2) < y * (2 * x + 1) ->
+  x * (z + y + 2) < y * (3 * x + 1).
+Proof.
+intros.
+pols.
+auto.
+Qed.
+
+Theorem polf_test1 x y :
+  0 <= x -> 1 <= y -> x <= x * y.
+Proof.
 intros.
 polf.
 Qed.
 
-Theorem polf_test2: forall x y, (0 < x -> x  <= x  * y -> 1 <= y)%R.
-intros.
-hyp_polf H0.
+Theorem polf_test2 x y :
+  0 < x -> x <= x * y -> 1 <= y.
+Proof.
+intros H1 H2.
+hyp_polf H2.
 Qed.
 
-Theorem polr_test1: forall x y z, (x + z) < y -> x + y + z < 2*y.
-intros x y z H.
+Theorem polr_test1 x y z :
+  x + z < y ->
+  x + y + z < 2 * y.
+Proof.
+intros H.
 polr H.
 pols.
 auto.
@@ -56,15 +72,17 @@ pols.
 auto with real.
 Qed.
 
-Theorem polr_test2: forall x y z t u, t <0 -> y = u -> x + z <  y ->  2* y * t <  x * t + t * u + z * t .
-intros x y z t u H H1 H2.
+Theorem polr_test2 x y z t u :
+  t < 0 -> y = u ->
+  x + z < y ->
+  2 * y * t < x * t + t * u + z * t.
+Proof.
+intros H1 H2 H3.
 polf.
-polr H1; auto with real.
-polr H2.
+polr H2; auto with real.
+polr H3.
 pols.
 auto.
 pols.
 auto with real.
 Qed.
-
-
