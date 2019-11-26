7eb8273764f1d0df19e47f78bf4eea52be46f986
thery <Laurent.Thery@inria.fr>
2019-11-27 00:37:06 +0100

    8.10

--- NGroundTac.v
+++ NGroundTac.v
@@ -13,7 +13,7 @@ Qed.
 
 Close Scope N_scope.
 
-Hint Extern 4 (N.lt ?X1 ?X2) => exact (refl_equal Lt).
-Hint Extern 4 (N.le ?X1 ?X2) => exact Lt_diff_Gt || exact Eq_diff_Gt.
+Hint Extern 4 (N.lt ?X1 ?X2) => exact (refl_equal Lt) : core.
+Hint Extern 4 (N.le ?X1 ?X2) => exact Lt_diff_Gt || exact Eq_diff_Gt : core.
 
 
--- NatGroundTac.v
+++ NatGroundTac.v
@@ -22,5 +22,7 @@ Theorem lt_correct: forall n m, lt_bool n m = true -> n <= m.
 induction n; destruct m; simpl; auto with arith; intros; discriminate.
 Qed.
 
-Hint Extern 4 (?X1 <= ?X2)%nat => exact (le_correct X1 X2 (refl_equal true)).
-Hint Extern 4 (?X1 < ?X2)%nat => exact (lt_correct X1 X2 (refl_equal true)).
+Hint Extern 4 (?X1 <= ?X2)%nat => 
+   exact (le_correct X1 X2 (refl_equal true)) : core.
+Hint Extern 4 (?X1 < ?X2)%nat => 
+   exact (lt_correct X1 X2 (refl_equal true)) : core.
